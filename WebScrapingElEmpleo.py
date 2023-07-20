8b4b070a0ef84d5b4b69d53e53050b038cdf31e3
Camilo Bustos <bu_ivan@javeriana.edu.co>
2023-07-20 13:43:03 -0500

    ya lo coge casi todo, pero le falata que coja las cosas que no tienen class en la etiqueta

--- WebScrapingElEmpleo.py
+++ WebScrapingElEmpleo.py
@@ -4,10 +4,13 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from clases import *
 
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import colorama as c
-import time 
+import time
+
+from clases.raw_offer import raw_offer 
 
 
 #opciones de navegación 
@@ -21,12 +24,13 @@ options.add_argument('--log-level=3')
 #options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
 
 tSleep1 = 1
-
+lista_offertas = []
 listaClases = []
 lista_Elementos_depurados = []
 
 #inizializar navegador 
-url = 'https://www.elempleo.com/co/ofertas-empleo/hace-1-semana?trabajo=Bogota'
+url = 'https://www.elempleo.com/co/ofertas-empleo/bogota/hace-1-semana'
+#https://www.elempleo.com/co/ofertas-empleo/bogota/hace-1-semana
 
 driver = webdriver.Chrome(options=options)
 driver.get(url)
@@ -48,37 +52,49 @@ for div in div_elements:
     if  "EMPRESA CONFIDENCIAL" not in div.text.upper()  and "SALARIO A CONVENIR" not in div.text.upper() :
         if "Bogotá" in lineas[2]:
             lista_Elementos_depurados.append(div)
-
-#ya vamos a trabajar sobre la lista de los empleos validos 
-for div in lista_Elementos_depurados:
-    texto_div = div.text    
-      
-
     
-
 
 #esta parte del codigo nos muestra como pasar a otra pagina 
 try:
    for div in lista_Elementos_depurados:
+        aux = raw_offer
         texto_div = div.text
         lineas = texto_div.split('\n')
         enlace = div.find_element(By.CSS_SELECTOR, 'a.text-ellipsis.js-offer-title' )
         driver.execute_script("arguments[0].scrollIntoView();", enlace)
         enlace.send_keys(Keys.CONTROL + Keys.RETURN)
-        print(texto_div) 
+        #print(texto_div) 
         
-        time.sleep(tSleep1)
+        time.sleep(1)
         ventanas_abiertas = driver.window_handles
         driver.switch_to.window(ventanas_abiertas[1])
-        time.sleep(1)
-        
+        time.sleep(2)
+        aux = raw_offer(None, None,None, None, None, None, None,None  )
         #hacer la recolección de datos de las paginas 
-            
-   
+        title = driver.find_element(By.CSS_SELECTOR, 'span.js-jobOffer-title')
+        company = driver.find_element(By.CSS_SELECTOR, 'h2.ee-mod.ee-wrap-text.ee-offer-company-title.js-company-name')
+        description = driver.find_element(By.CSS_SELECTOR, 'div.description-block')
+        salary = driver.find_element(By.CSS_SELECTOR, 'span.js-joboffer-salary')
+        education = driver.find_element(By.CSS_SELECTOR, 'span.js-education-level')
+        #ux.experience = driver.find_element(By.CSS_SELECTOR, '')#no tienen nombre de itqueta 
+        #aux.contract = driver.find_element(By.CSS_SELECTOR, '')#tampoco hay etiqueta 
+        date = driver.find_element(By.CSS_SELECTOR, 'span.js-publish-date')
         #cambiar a pestaña 1 de vuelta 
+        #print(title)
+        texto_titulo = title.text
+        texto_empresa = company.text
+        texto_descripcion = description.text
+        texto_salario = salary.text
+        texto_educacion = education.text
+        texto_fecha = date.text
+        aux = raw_offer(texto_titulo, texto_empresa,texto_descripcion, texto_salario, texto_educacion, None, None,texto_fecha  )
+        print(aux)
+        print(texto_salario)
+        print()
+        print()
         driver.close()
         driver.switch_to.window(ventanas_abiertas[0]) 
-        time.sleep(1)
+        time.sleep(3)
    
    
 
