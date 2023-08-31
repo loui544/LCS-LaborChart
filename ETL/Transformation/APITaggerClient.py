f192c00bea06f45b26d80d666215bf2fc8f31992
loui544 <loui_vel@hotmail.com>
2023-08-31 14:49:26 -0500

    Update transformation

--- ETL/Transformation/APITaggerClient.py
+++ ETL/Transformation/APITaggerClient.py
@@ -1,52 +1,39 @@
 import requests
 import json
 
-def get_offers_with_last_date(filename):
-    try:
-        with open(filename, 'r',encoding='utf-8') as file:
-            data = json.load(file)
-
-        if data:
-            # Ordenar las ofertas por fecha en orden descendente
-            sorted_data = sorted(data, key=lambda x: x['date'], reverse=True)
-            
-            # Obtener la fecha de la última oferta
-            last_date = sorted_data[0]['date']
-            
-            # Filtrar las ofertas que tienen la misma fecha que la última oferta
-            offers_with_last_date = [offer for offer in sorted_data if offer['date'] == last_date]
-            
-            # Obtener solo las descripciones de las ofertas
-            descriptions = [{"description":offer['description']} for offer in offers_with_last_date]
-            
-            return descriptions
-        
-        return []
     
-    except FileNotFoundError:
-        print("El archivo no se encontró.")
-        return []
-    except Exception as e:
-        print("Error:", e)
-        return []
 
-# Nombre del archivo offers.json
-filename = 'offers.json'
+# File where are the offers
+filename = 'ETL\Extraction\offers.json'
+try:
+    with open(filename, 'r',encoding='utf-8') as file:
+        data = json.load(file)
 
-# Datos en formato JSON que deseas enviar al servidor
-data= get_offers_with_last_date(filename)
+    if data:
+        # Obtener solo las descripciones de las ofertas
+        descriptions = [{"description":offer['description']} for offer in data]
+    
+except FileNotFoundError:
+    print("El archivo no se encontró.")
+except Exception as e:
+    print("Error:", e)
 
 # URL del servidor API
 api_url = 'http://localhost:5000/retrieveDescriptions'
 
 try:
-    # Envía la solicitud POST con los datos JSON al servidor
-    response = requests.post(api_url, json=data)
-    # Verifica el código de respuesta
+    # 
+    response = requests.post(api_url, json=descriptions)
+    # 
     if response.status_code == 200:
         result = response.json()
-        with open('desc.json', 'w', encoding='utf-8') as desc_file:
-            json.dump(result, desc_file, indent=4, ensure_ascii=False)
+        
+        for idx, item in enumerate(data):
+            if str(idx) in result:
+                data[idx]["entities"] = result[str(idx)]["entities"]
+
+        with open('ETL\Transformation\desc.json', 'w', encoding='utf-8') as desc_file:
+            json.dump(data, desc_file, indent=4, ensure_ascii=False)
         print("Respuesta del servidor guardada en 'desc.json'")
     else:
         print("Error en la solicitud. Código de respuesta:", response.status_code)
