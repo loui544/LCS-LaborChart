d91af54069920bf665ab10f8a126adffc5d01b7c
Santiago Burbano <88295200+SantoB20@users.noreply.github.com>
2023-10-04 23:39:37 -0500

    update

--- pruebaExtraccion.py
+++
@@ -1,5 +0,0 @@
-from ETL.Extraction.ElEmpleoExtraction.WebScraperElEmpleo import webScraperElempleo
-from ETL.Extraction.ElEmpleoExtraction.NormalizerElEmpleo import offersNormalizer
-
-
-offersNormalizer(webScraperElempleo())
