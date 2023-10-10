a754de0d4c05a06f1556f0679ce25d5ed8800787
Santiago Burbano <88295200+SantoB20@users.noreply.github.com>
2023-10-10 13:00:50 -0500

    update: deletes special characters

--- ETL/Classes/RawOffer.py
+++ ETL/Classes/RawOffer.py
@@ -1,3 +1,6 @@
+import re
+
+
 class rawOffer:
     def __init__(self, title, company, description, salary, education, experience, contract, date):
         self.title = title
@@ -10,9 +13,12 @@ class rawOffer:
         self.date = date
 
     def lowerCase(self):
-        self.title = self.title.lower()
-        self.company = self.company.lower()
-        self.description = self.description.lower()
+        self.title = re.sub(
+            '[^a-zA-Z0-9 áéíóúÁÉÍÓÚüñ \n\.]', '', self.title.lower().replace('\n', ' ').replace('.', ''))
+        self.company = re.sub(
+            '[^a-zA-Z0-9 áéíóúÁÉÍÓÚüñ \n\.]', '', self.company.lower().replace('\n', ' ').replace('.', ''))
+        self.description = re.sub(
+            '[^a-zA-Z0-9 áéíóúÁÉÍÓÚüñ \n\.]', '', self.description.lower().replace('\n', ' ').replace('.', ''))
 
     def __str__(self):
         return 'Nombre: {0}\nEmpresa: {1}\nDescripcción: {2}\nSalario: {3}\nNivel educativo: {4}\nExperiencia requerida: {5}\nTipo de contrato: {6}\nFecha de publicación: {7}\n'.format(self.title, self.company, self.description, self.salary, self.education, self.experience, self.contract, self.date)
